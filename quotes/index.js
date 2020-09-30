ad024d82b42216f38f81e2b2f40a23c79e6c9e5c
LarsVomMars <lars@kroenner.eu>
2020-09-30 20:37:16 +0200

    One project one server

--- quotes/index.js
+++ quotes/index.js
@@ -1,28 +1,6 @@
-require("dotenv").config();
 const express = require("express");
-const mariadb = require("mariadb");
-const app = express();
-const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-});
-
-let db_conn;
-
-pool.getConnection()
-    .then((conn) => {
-        db_conn = conn;
-        db_conn.release();
-    })
-    .catch((err) => {
-        console.error(err);
-        db_conn = null;
-    });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const db = require("../db");
+const app = express.Router();
 
 app.use("/", express.static(__dirname + "/public"));
 
@@ -30,9 +8,4 @@ app.get("/api/list", (req, res) => {
     res.send("ok\n");
 });
 
-app.on("close", () => {
-    console.log("CLOSE");
-});
-
-console.log("Listening on port 5007");
-app.listen(5007);
+module.exports = app;
