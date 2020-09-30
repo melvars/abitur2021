3491013c43775401b5e87f97c09d906de5bafbc9
Marvin Borner <develop@marvinborner.de>
2020-09-30 21:01:13 +0200

    Lars is dumb

--- tables.sql
+++ tables.sql
@@ -1,7 +1,11 @@
+set global innodb_large_prefix = ON;
+
 CREATE TABLE IF NOT EXISTS theme(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    main TEXT NOT NULL UNIQUE,
-    description TEXT NOT NULL UNIQUE,
+    main VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     votes INTEGER DEFAULT FALSE,
-    hidden BOOLEAN DEFAULT FALSE
-);
+    hidden BOOLEAN DEFAULT FALSE,
+
+    UNIQUE KEY main (main, description)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
