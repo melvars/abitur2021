c252e76dd665ddd0cfa84ffdaa26fb52f0796600
Marvin Borner <develop@marvinborner.de>
2020-10-02 13:58:22 +0200

    Added oisjgoiesjgoseijgf

--- quotes/index.js
+++ quotes/index.js
@@ -22,7 +22,7 @@ app.post("/api/add", checkUser, async (req, res) => {
 
 app.get("/api/list", checkUser, async (req, res) => {
     const quotes = await db.query(
-        "SELECT quotes.id, name, middlename, surname, quote FROM quotes INNER JOIN users AS a ON author_id = a.id ORDER BY name"
+        "SELECT q.id, a.name, a.middlename, a.surname, q.quote, c.name AS class FROM quotes AS q INNER JOIN users AS a ON author_id = a.id INNER JOIN class AS c ON a.class_id = c.id ORDER BY a.name"
     );
     res.json(quotes);
 });
--- quotes/public/script.js
+++ quotes/public/script.js
@@ -1,5 +1,4 @@
 const dropdown = document.getElementById("author");
-const quotes = document.getElementById("quotes");
 
 dropdown.insertAdjacentHTML("beforeend", '<option selected="true" disabled>Author auswählen...</option>');
 
@@ -16,12 +15,14 @@ function appendOption(response) {
 
 function appendQuote(response) {
     response.forEach((elem) => {
-        quotes.insertAdjacentHTML(
-            "beforeend",
-            `<li>${elem["name"]} ${elem["middlename"] ? elem["middlename"] : " "}${elem["surname"]}: ${
-                elem["quote"]
-            }</li>`
-        );
+        document
+            .getElementById(elem["class"])
+            .insertAdjacentHTML(
+                "beforeend",
+                `<li>${elem["name"]} ${elem["middlename"] ? elem["middlename"] : " "}${elem["surname"]}: ${
+                    elem["quote"]
+                }</li>`
+            );
     });
 }
 
@@ -32,3 +33,12 @@ fetch("/auth/api/list")
 fetch("/quotes/api/list")
     .then((response) => response.json())
     .then((response) => appendQuote(response));
+
+const classes = ["TGI13.1", "TGI13.2", "TGM13.1", "TGM13.2", "TGTM13.1"];
+classes.forEach((clazz) => {
+    document.getElementById("open_" + clazz).addEventListener("click", () => {
+        const ul = document.getElementById(clazz);
+        if (ul.style.display === "none") ul.style.display = "block";
+        else ul.style.display = "none";
+    });
+});
