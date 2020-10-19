4194f192b4e175c4abd37d093850a77efd09c8bc
LarsVomMars <lars@kroenner.eu>
2020-10-19 21:56:52 +0200

    Some stuff

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.use("/", express.static(__dirname + "/overview/public"));
 app.use("/mottovote", checkUser, mottovote);
 app.use("/quotes", checkUser, quotes);
 // app.use("/poll", checkUser, poll);
-app.use("/profile", checkUser, profile);
+// app.use("/profile", checkUser, profile);
 app.use("/admin", checkAdmin, admin);
 app.use("/auth", auth);
 
-app.listen(process.env.PORT || 5005, () => console.log("Server started on http://localhost:5005"));
+app.listen(process.env.PORT || 5005, () => console.log(`Server started on http://localhost:${process.env.PORT}`));
