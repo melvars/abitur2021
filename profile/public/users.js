07aff0fad3eb67d826d17f9ecc0d236ad4f3829e
Marvin Borner <develop@marvinborner.de>
2021-01-29 17:30:07 +0100

    Minor improvements

--- profile/public/users.js
+++ profile/public/users.js
@@ -1,10 +1,20 @@
 function addUser(user) {
-    const li = document.createElement("li");
+    const li = document.createElement("a");
+    li.classList.add("pure-menu-link");
     li.textContent = `${user.name} ${user.middlename || ""} ${user.surname}`;
-    li.addEventListener("click", () => window.location.assign(`./user.html?uid=${user.id}`));
+    li.href = "./user.html?uid=" + user.id;
     if (user.class_id < 6) document.getElementById("class_" + user.class_id).appendChild(li);
 }
 
 fetch("/auth/api/list?class=all")
     .then((response) => response.json())
     .then((response) => response.forEach(addUser));
+
+document.querySelectorAll("b").forEach((elem) => {
+    const next = elem.nextElementSibling;
+    next.style.display = "none";
+    elem.addEventListener("click", () => {
+        console.log(elem);
+        next.style.display = next.style.display == "none" ? "block" : "none";
+    });
+});
