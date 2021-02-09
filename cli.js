a096977db64ff0d7eba706f9f791ce97c32156b4
Marvin Borner <develop@marvinborner.de>
2021-02-09 16:41:14 +0100

    basic emoji support

--- cli.js
+++ cli.js
@@ -74,6 +74,10 @@ if ((idx = params.indexOf("-r")) > -1) {
             .replace(/&/g, "\\&")
             .replace(/_/g, "\\_")
             .replace(/\^/g, "\\^")
+            .replace(
+                /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g,
+                "{ \\emojifont $&}",
+            )
             .replace(/~/g, "$\\mathtt{\\sim}$");
 
     let hay;
@@ -197,16 +201,16 @@ if ((idx = params.indexOf("-r")) > -1) {
 
             // Quotes boats coats floats goats oats
             textex = "";
-            textex += `\\subsection*{${data.quotes[0].class}}\n`
+            textex += `\\subsection*{${data.quotes[0].class}}\n`;
             data.quotes.forEach((quote, i) => {
-                if (i > 1 && quote.class !== data.quotes[i-1].class) textex += `\\subsection*{${quote.class}}\n`;
-                textex += `\\textbf{${quote.name} ${quote.middlename || ""} ${quote.surname}}: ${quote.quote}\\newline \n`.replace(/ +/, " ").replace(/%/g, "\\%");
-           
+                if (i > 1 && quote.class !== data.quotes[i - 1].class) textex += `\\subsection*{${quote.class}}\n`;
+                textex += `\\textbf{${quote.name} ${quote.middlename || ""} ${quote.surname}}: ${
+                    quote.quote
+                }\\newline \n`
+                    .replace(/ +/, " ")
+                    .replace(/%/g, "\\%");
             });
-            await fs.writeFile(
-                __dirname + "/zeitung/parts/generated/quotes/quotes.tex",
-                textex,
-            );
+            await fs.writeFile(__dirname + "/zeitung/parts/generated/quotes/quotes.tex", textex);
         });
     });
     console.log("Probably finished?");
