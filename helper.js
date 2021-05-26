1f31023a1c8bf0c21b836e8d7435ce07858d9cf7
Marvin Borner <develop@marvinborner.de>
2021-05-26 20:54:22 +0200

    Fixed lars

--- helper.js
+++ helper.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 require("dotenv").config();
 const db = require("./db");
 const fs = require("fs").promises;
@@ -6,8 +7,11 @@ async function main() {
     let studs = await fs.readFile(__dirname + "/studs.csv", "utf8");
     const users = await db.query("SELECT id, username, name, surname FROM users");
     for (const user of users) {
-        studs = studs.replaceAll(`;${user.username};`, `;${user.id};`);
-        studs = studs.replaceAll(`${user.username};`, `${user.name} ${user.surname};`);
+        let rep = `${user.username};`;
+        const re1 = new RegExp(";" + rep, "g");
+        studs = studs.replace(re1, `;${user.id};`);
+        const re2 = new RegExp(rep, "g");
+        studs = studs.replace(re2, `${user.name} ${user.surname};`);
     }
     await fs.writeFile(__dirname + "/studsfix.csv", studs, "utf8");
     const json = await ssvToObj(studs);
